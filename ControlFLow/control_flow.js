84ef11288b343b2d763c9a293d6f717824e41706
Pushkal26 <burdepushkal.prb113@gmail.com>
2024-08-18 08:50:24 -0400

    message

---
+++ ControlFLow/control_flow.js
@@ -0,0 +1,46 @@
+let userRole ="admin";
+let accessLevel;
+
+if(userRole=='admin'){
+    accessLevel= "Full access granted";
+}else if (userRole == 'manager'){
+    accessLevel = "Limited access granted";
+}else{
+    accessLevel = "No access granted";
+}
+
+console.log(" Access Level ",accessLevel)
+
+let isLoggedIn = true;
+let userMessage;
+
+if(isLoggedIn){
+    if(userRole =='admin'){
+        userMessage = "Welcome admin";
+    }else{
+        userMessage = "Welcome user";
+    }
+}else{
+    userMessage = "Please log in to access the system ";
+}
+
+console.log(" User message", userMessage);
+
+let userType = "subscriber";
+let userCatergory;
+
+switch(userType){
+    case "admin":
+        userCatergory = "Administrator";
+        break;
+    case "manager":
+        userCatergory = "Manager";
+        break;
+    case "subscriber":
+        userCatergory = "Subscriber";
+        break;
+    default:
+        userCatergory ="unknown";
+}
+
+console.log("User Category: ",userCatergory);
